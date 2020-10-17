5b245f0d826edade66e3fc14e38918e058f587f0
David Leger <davidleger95@me.com>
2020-10-17 17:41:38 -0300

    feat(added card text and image components, and some sytling): added card text and image components

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  position: sticky;
+  top: 0;
+  background: #fff;
+  padding: 1rem;
+`;
+
+const Title = styled.h1`
+  font-size: 1.25rem;
+  margin: 0;
+  text-align: center;
+`;
+
+const Header: FC = () => (
+  <StyledHeader>
+    <Title>David Leger</Title>
+  </StyledHeader>
+);
+
+export default Header;
